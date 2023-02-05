ef59635689728bffb20806b67d1f6d72352d2c5f
Julien Labbé <Julien.Labbe@univ-grenoble-alpes.fr>
2023-02-05 15:12:17 +0100

    Add emacs bookmarks

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((nil .
+      ((eval .
+       ;; bookmarks
+       (setq bookmark-default-file (concat (doom-project-root) ".emacs-bookmarks"))))))
