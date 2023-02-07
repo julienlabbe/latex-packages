6cfb342363de24c39896c078d8c313178987a1cb
Julien Labbé <Julien.Labbe@univ-grenoble-alpes.fr>
2023-02-07 08:45:55 +0100

    Remove emacs config from git

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-((nil .
-      ((eval .
-       ;; bookmarks
-       (setq bookmark-default-file (concat (doom-project-root) ".emacs-bookmarks"))))))
